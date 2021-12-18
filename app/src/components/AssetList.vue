bca42f196a2b3d8363c92d280c4ff8a692ebaeea
Stefan Haselwanter <stefan.haselwanter@gmail.com>
2021-12-18 14:08:07 +0100

    Create filter component

--- app/src/components/AssetList.vue
+++ app/src/components/AssetList.vue
@@ -8,15 +8,15 @@ import Asset from "./Asset.vue";
 import AlertMessage from "./AlertMessage.vue";
 import LoadingBar from "./LoadingBar.vue";
 import Pagination from "./Pagination.vue";
+import Filter from "./Filter.vue";
 
 let assets = ref<IAsset[]>();
 let filtered = ref<IAsset[]>();
-const search = ref("");
 const error = ref(false);
 
-const filterBy = (event: Event) => {
+const filterBy = (search: string) => {
     filtered.value = assets.value?.filter((asset) => {
-        const searchString = search.value.toLowerCase();
+        const searchString = search.toLowerCase();
 
         return asset.name.toLowerCase().includes(searchString) || asset.symbol.toLowerCase().includes(searchString);
     });
@@ -52,13 +52,7 @@ onMounted(() => {
     <AlertMessage v-if="error">No assets found!</AlertMessage>
 
     <div v-else>
-        <input
-            v-model="search"
-            @input.prevent="filterBy"
-            type="search"
-            placeholder="Search..."
-            class="form-input w-full md:w-1/2 mb-6"
-        />
+        <Filter @filter="filterBy" />
 
         <Pagination @update="getAssets" />
 
---
+++ app/src/components/Filter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const search = ref("");
+</script>
+
+<template>
+    <div>
+        <input
+            v-model="search"
+            @input.prevent="$emit('filter', search)"
+            type="search"
+            placeholder="Search..."
+            class="form-input w-full md:w-1/2 mb-6"
+        />
+    </div>
+</template>
